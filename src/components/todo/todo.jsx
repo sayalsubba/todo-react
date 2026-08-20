7305031d4bc8e06514dbf3120a81b8e6c2ccfd31
Sayal Subba <sahilphago23@gmail.com>
2026-08-20 23:44:45 +0545

    localstorage logic fix

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -2,13 +2,17 @@ import React, { createElement, useEffect, useState } from 'react'
 import Css from "./todo.module.css"
 export default function todo() {
     const [input, setInput] = useState("");
-    const [list, setList] = useState(localStorage.getItem("save") || []);
+    const [list, setList] = useState(JSON.parse(localStorage.getItem("save")) || [])
     useEffect(() => {
         localStorage.setItem("save", JSON.stringify(list));
     }, [list]);
     const handleAdd = () => {
         setList((prev) => [...prev, input]);
         setInput("");
+
+    }
+    const selectItem =(item)=>{
+
     }
     const handleRemove = (item) => {
         setList(list.filter((list) => list !== item));
@@ -23,10 +27,14 @@ export default function todo() {
 
                     <button onClick={handleAdd}>Add</button>
                     <ul>
-                        {list.map((item,index) => {
 
-                        })}
-
+                        {list.map((item, index) => (
+                            <li key={item} onClick={() => {
+                                selectItem(item)
+                            }}>{item} <span onClick={() => {
+                                handleRemove(item)
+                            }} key={item}>🅧</span></li>
+                        ))}
                     </ul>
 
                 </div>
