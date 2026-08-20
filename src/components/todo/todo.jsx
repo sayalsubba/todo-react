cbd2fca9ca263d46da88d838e45dd6a087c5c11f
Sayal Subba <sahilphago23@gmail.com>
2026-08-20 21:47:12 +0545

    adding localstorage

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -2,27 +2,30 @@ import React, { createElement, useEffect, useState } from 'react'
 import Css from "./todo.module.css"
 export default function todo() {
     const [input, setInput] = useState("");
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(localStorage.getItem("save") || []);
+   
+    useEffect(() => {
+        localStorage.setItem("save", JSON.stringify(list));
+    }, [list]);
     const handleAdd = () => {
         setList((prev) => [...prev, input]);
         setInput("");
     }
-   const handleRemove =(item)=>{
-    setList(list.filter((list)=> list !== item));
-   }
+    const handleRemove = (item) => {
+        setList(list.filter((list) => list !== item));
+    }
     return (
         <>
             <div className={Css["main"]}>
                 <div id='mainId' className={Css["mainPrt"]}>
                     <input type="text" value={input} placeholder='Enter Task' onChange={(e) => {
-                        setInput(e.target.value);
-                    }}
-                    />
+                        setInput(e.target.value); }} />
+
                     <button onClick={handleAdd}>Add</button>
                     <ul>
-                        {list.map((item, index) =>
-                            <li key={index}>{item}<span key={item} onClick={()=>handleRemove(item)}>🅧</span></li>
-                        )}
+                     {list.map(()=>{
+                        
+                     })}
 
                     </ul>
 
